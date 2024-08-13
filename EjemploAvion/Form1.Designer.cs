449254c78e799312eca67a8542cc7f218ff7dada
MarioRomeroTTP <mario.romero@totalpack.cl>
2024-08-13 00:33:18 -0400

    Se termkna de recoger los atributos del objeto de tipo Avión y se muestran en un mensaje

--- EjemploAvion/Form1.Designer.cs
+++ EjemploAvion/Form1.Designer.cs
@@ -28,172 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBoxModelo = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.numericUpDownTiempoDeServicio = new System.Windows.Forms.NumericUpDown();
-            this.SI = new System.Windows.Forms.RadioButton();
-            this.NO = new System.Windows.Forms.RadioButton();
-            this.groupBoxActivo = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBoxAniosDeServicio = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBoxCantidadPasajeros = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.dateTimePickerPrimerVuelo = new System.Windows.Forms.DateTimePicker();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTiempoDeServicio)).BeginInit();
-            this.groupBoxActivo.SuspendLayout();
-            this.SuspendLayout();
+            label1 = new Label();
+            textBoxModelo = new TextBox();
+            label2 = new Label();
+            numericUpDownTiempoDeServicio = new NumericUpDown();
+            activo = new RadioButton();
+            noActivo = new RadioButton();
+            groupBoxActivo = new GroupBox();
+            label3 = new Label();
+            textBoxAniosDeServicio = new TextBox();
+            label4 = new Label();
+            textBoxCantidadPasajeros = new TextBox();
+            label5 = new Label();
+            dateTimePickerPrimerVuelo = new DateTimePicker();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownTiempoDeServicio).BeginInit();
+            groupBoxActivo.SuspendLayout();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(98, 61);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Modelo";
+            label1.AutoSize = true;
+            label1.Location = new Point(112, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Modelo";
             // 
             // textBoxModelo
             // 
-            this.textBoxModelo.Location = new System.Drawing.Point(220, 58);
-            this.textBoxModelo.Name = "textBoxModelo";
-            this.textBoxModelo.Size = new System.Drawing.Size(100, 23);
-            this.textBoxModelo.TabIndex = 1;
+            textBoxModelo.Location = new Point(251, 77);
+            textBoxModelo.Margin = new Padding(3, 4, 3, 4);
+            textBoxModelo.Name = "textBoxModelo";
+            textBoxModelo.Size = new Size(114, 27);
+            textBoxModelo.TabIndex = 1;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(98, 102);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 15);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Tiempo de servicio";
+            label2.AutoSize = true;
+            label2.Location = new Point(112, 136);
+            label2.Name = "label2";
+            label2.Size = new Size(135, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Tiempo de servicio";
             // 
             // numericUpDownTiempoDeServicio
             // 
-            this.numericUpDownTiempoDeServicio.Location = new System.Drawing.Point(220, 100);
-            this.numericUpDownTiempoDeServicio.Name = "numericUpDownTiempoDeServicio";
-            this.numericUpDownTiempoDeServicio.Size = new System.Drawing.Size(120, 23);
-            this.numericUpDownTiempoDeServicio.TabIndex = 2;
+            numericUpDownTiempoDeServicio.Location = new Point(251, 133);
+            numericUpDownTiempoDeServicio.Margin = new Padding(3, 4, 3, 4);
+            numericUpDownTiempoDeServicio.Name = "numericUpDownTiempoDeServicio";
+            numericUpDownTiempoDeServicio.Size = new Size(137, 27);
+            numericUpDownTiempoDeServicio.TabIndex = 2;
             // 
-            // SI
+            // activo
             // 
-            this.SI.AutoSize = true;
-            this.SI.Location = new System.Drawing.Point(14, 22);
-            this.SI.Name = "SI";
-            this.SI.Size = new System.Drawing.Size(34, 19);
-            this.SI.TabIndex = 3;
-            this.SI.TabStop = true;
-            this.SI.Text = "SI";
-            this.SI.UseVisualStyleBackColor = true;
+            activo.AutoSize = true;
+            activo.Location = new Point(16, 29);
+            activo.Margin = new Padding(3, 4, 3, 4);
+            activo.Name = "activo";
+            activo.Size = new Size(42, 24);
+            activo.TabIndex = 3;
+            activo.TabStop = true;
+            activo.Text = "SI";
+            activo.UseVisualStyleBackColor = true;
             // 
-            // NO
+            // noActivo
             // 
-            this.NO.AutoSize = true;
-            this.NO.Location = new System.Drawing.Point(71, 22);
-            this.NO.Name = "NO";
-            this.NO.Size = new System.Drawing.Size(43, 19);
-            this.NO.TabIndex = 3;
-            this.NO.TabStop = true;
-            this.NO.Text = "NO";
-            this.NO.UseVisualStyleBackColor = true;
+            noActivo.AutoSize = true;
+            noActivo.Location = new Point(106, 29);
+            noActivo.Margin = new Padding(3, 4, 3, 4);
+            noActivo.Name = "noActivo";
+            noActivo.Size = new Size(52, 24);
+            noActivo.TabIndex = 3;
+            noActivo.TabStop = true;
+            noActivo.Text = "NO";
+            noActivo.UseVisualStyleBackColor = true;
             // 
             // groupBoxActivo
             // 
-            this.groupBoxActivo.Controls.Add(this.SI);
-            this.groupBoxActivo.Controls.Add(this.NO);
-            this.groupBoxActivo.Location = new System.Drawing.Point(98, 142);
-            this.groupBoxActivo.Name = "groupBoxActivo";
-            this.groupBoxActivo.Size = new System.Drawing.Size(200, 66);
-            this.groupBoxActivo.TabIndex = 5;
-            this.groupBoxActivo.TabStop = false;
-            this.groupBoxActivo.Text = "Está activo";
+            groupBoxActivo.Controls.Add(activo);
+            groupBoxActivo.Controls.Add(noActivo);
+            groupBoxActivo.Location = new Point(112, 189);
+            groupBoxActivo.Margin = new Padding(3, 4, 3, 4);
+            groupBoxActivo.Name = "groupBoxActivo";
+            groupBoxActivo.Padding = new Padding(3, 4, 3, 4);
+            groupBoxActivo.Size = new Size(229, 88);
+            groupBoxActivo.TabIndex = 5;
+            groupBoxActivo.TabStop = false;
+            groupBoxActivo.Text = "Está activo";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(374, 66);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 15);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Años de servicio";
+            label3.AutoSize = true;
+            label3.Location = new Point(427, 88);
+            label3.Name = "label3";
+            label3.Size = new Size(117, 20);
+            label3.TabIndex = 0;
+            label3.Text = "Años de servicio";
             // 
             // textBoxAniosDeServicio
             // 
-            this.textBoxAniosDeServicio.Location = new System.Drawing.Point(482, 63);
-            this.textBoxAniosDeServicio.Name = "textBoxAniosDeServicio";
-            this.textBoxAniosDeServicio.Size = new System.Drawing.Size(53, 23);
-            this.textBoxAniosDeServicio.TabIndex = 1;
+            textBoxAniosDeServicio.Location = new Point(551, 84);
+            textBoxAniosDeServicio.Margin = new Padding(3, 4, 3, 4);
+            textBoxAniosDeServicio.Name = "textBoxAniosDeServicio";
+            textBoxAniosDeServicio.Size = new Size(60, 27);
+            textBoxAniosDeServicio.TabIndex = 1;
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(360, 102);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(107, 15);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Cantidad pasajeros";
+            label4.AutoSize = true;
+            label4.Location = new Point(411, 136);
+            label4.Name = "label4";
+            label4.Size = new Size(136, 20);
+            label4.TabIndex = 0;
+            label4.Text = "Cantidad pasajeros";
             // 
             // textBoxCantidadPasajeros
             // 
-            this.textBoxCantidadPasajeros.Location = new System.Drawing.Point(482, 99);
-            this.textBoxCantidadPasajeros.Name = "textBoxCantidadPasajeros";
-            this.textBoxCantidadPasajeros.Size = new System.Drawing.Size(53, 23);
-            this.textBoxCantidadPasajeros.TabIndex = 1;
+            textBoxCantidadPasajeros.Location = new Point(551, 132);
+            textBoxCantidadPasajeros.Margin = new Padding(3, 4, 3, 4);
+            textBoxCantidadPasajeros.Name = "textBoxCantidadPasajeros";
+            textBoxCantidadPasajeros.Size = new Size(60, 27);
+            textBoxCantidadPasajeros.TabIndex = 1;
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(329, 166);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 15);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "PrimerVuelo";
+            label5.AutoSize = true;
+            label5.Location = new Point(376, 221);
+            label5.Name = "label5";
+            label5.Size = new Size(90, 20);
+            label5.TabIndex = 0;
+            label5.Text = "PrimerVuelo";
             // 
             // dateTimePickerPrimerVuelo
             // 
-            this.dateTimePickerPrimerVuelo.Location = new System.Drawing.Point(442, 164);
-            this.dateTimePickerPrimerVuelo.Name = "dateTimePickerPrimerVuelo";
-            this.dateTimePickerPrimerVuelo.Size = new System.Drawing.Size(160, 23);
-            this.dateTimePickerPrimerVuelo.TabIndex = 6;
+            dateTimePickerPrimerVuelo.Location = new Point(505, 219);
+            dateTimePickerPrimerVuelo.Margin = new Padding(3, 4, 3, 4);
+            dateTimePickerPrimerVuelo.Name = "dateTimePickerPrimerVuelo";
+            dateTimePickerPrimerVuelo.Size = new Size(182, 27);
+            dateTimePickerPrimerVuelo.TabIndex = 6;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(291, 259);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Ingresar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            button1.Location = new Point(333, 345);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 31);
+            button1.TabIndex = 7;
+            button1.Text = "Ingresar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(645, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dateTimePickerPrimerVuelo);
-            this.Controls.Add(this.groupBoxActivo);
-            this.Controls.Add(this.numericUpDownTiempoDeServicio);
-            this.Controls.Add(this.textBoxCantidadPasajeros);
-            this.Controls.Add(this.textBoxAniosDeServicio);
-            this.Controls.Add(this.textBoxModelo);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTiempoDeServicio)).EndInit();
-            this.groupBoxActivo.ResumeLayout(false);
-            this.groupBoxActivo.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(737, 600);
+            Controls.Add(button1);
+            Controls.Add(dateTimePickerPrimerVuelo);
+            Controls.Add(groupBoxActivo);
+            Controls.Add(numericUpDownTiempoDeServicio);
+            Controls.Add(textBoxCantidadPasajeros);
+            Controls.Add(textBoxAniosDeServicio);
+            Controls.Add(textBoxModelo);
+            Controls.Add(label2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDownTiempoDeServicio).EndInit();
+            groupBoxActivo.ResumeLayout(false);
+            groupBoxActivo.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -202,8 +212,8 @@
         private TextBox textBoxModelo;
         private Label label2;
         private NumericUpDown numericUpDownTiempoDeServicio;
-        private RadioButton SI;
-        private RadioButton NO;
+        private RadioButton activo;
+        private RadioButton noActivo;
         private GroupBox groupBoxActivo;
         private Label label3;
         private TextBox textBoxAniosDeServicio;
--- EjemploAvion/Form1.cs
+++ EjemploAvion/Form1.cs
@@ -20,11 +20,32 @@ namespace EjemploAvion
 
             objetoAvion2.Modelo = textBoxModelo.Text;
 
+            //con (int) se convierte el valor de numericUpDownTiempoDeServicio a entero.
+            //Conversion explicita = quiere decir que se esta forzando la conversion
+            //desde un tipo de dato a otro. en este caso de decimal a entero.
             objetoAvion2.TiempoDeServicio = (int)numericUpDownTiempoDeServicio.Value;
 
-            objetoAvion2.EstaActivo = 
+            objetoAvion2.EstaActivo = activo.Checked ? true : false;
 
-            objetoAvion2.AniosDeServicio =  int.Parse(textBoxAniosDeServicio.Text);
+            //con int.Parse se convierte el valor de textBoxAniosDeServicio a entero.
+            //conversión mediante el método Parse = se convierte el valor de un tipo de dato a otro.
+            //en este caso de cadena a entero.
+            objetoAvion2.AniosDeServicio = int.Parse(textBoxAniosDeServicio.Text);
+
+            objetoAvion2.CantidadPasajeros = int.Parse(textBoxCantidadPasajeros.Text);
+
+            objetoAvion2.PrimerVuelo = dateTimePickerPrimerVuelo.Value;
+
+            MessageBox.Show("El modelo del avión es: " + objetoAvion2.Modelo + "\n" +
+                            "El tiempo de servicio del avión es: " + objetoAvion2.TiempoDeServicio + "\n" +
+                            "El avión está activo: " + objetoAvion2.EstaActivo + "\n" +
+                            "Los años de servicio del avión son: " + objetoAvion2.AniosDeServicio + "\n" +
+                            "La cantidad de pasajeros del avión es: " + objetoAvion2.CantidadPasajeros + "\n" +
+                            "El primer vuelo del avión fue: " + objetoAvion2.PrimerVuelo);
+
+            
+
+
 
 
 
